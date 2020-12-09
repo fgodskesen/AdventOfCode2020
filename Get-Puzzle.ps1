d86d5a7c90329ef52bf5605012b13d00714fab00
Flemming Godskesen <flgod@DFDS.COM>
2020-12-09 12:56:38 +0100

    commit to day 9

---
+++ Get-Puzzle.ps1
@@ -0,0 +1,27 @@
+﻿$day = 9
+
+
+$AoCCookies = Import-PowerShellDataFile 'C:\users\flgod\OneDrive - DFDS\PoSH\GIT\adventcookie.psd1'
+
+$session = [Microsoft.PowerShell.Commands.WebRequestSession]::new()
+$AoCCookies.Keys | ForEach-Object {
+    $cookie = [System.Net.Cookie]::new()
+    $cookie.Name = $_
+    $cookie.Value = $AoCCookies[$_]
+    $cookie.Domain = "adventofcode.com"
+    $session.Cookies.Add($cookie);
+}
+
+
+
+New-Item -ItemType Directory -Name "day$day" -force
+
+
+$downloadToPath = ".\day$day\puzzle.html"
+$remoteFileLocation = "https://adventofcode.com/2020/day/$day"
+Invoke-WebRequest $remoteFileLocation -WebSession $session -OutFile $downloadToPath
+
+
+$downloadToPath = ".\day$day\input.txt"
+$remoteFileLocation = "https://adventofcode.com/2020/day/$day/input"
+Invoke-WebRequest $remoteFileLocation -OutFile $downloadToPath -WebSession $session
